343b11a987062f34e7ac5bd28443cb09ec093375
Yaniv Kremer <yanivkr7@gmail.com>
2020-10-06 01:48:07 +0300

    Broken homepage to components and adding style

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
 const HomePage = () => (
 	<div className="homepage">
-		<div class="directory-menu">
+		<Directory />
+		{/* <div className="directory-menu">
 			<div className="menu-item">
 				<div className="content">
 					<h1 className="title">HATS</h1>
@@ -34,7 +36,7 @@ const HomePage = () => (
 					<span className="subtitle">SHOP NOW</span>
 				</div>
 			</div>
-		</div>
+		</div> */}
 	</div>
 );
 
