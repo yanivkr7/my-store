774beaedb34edb5c636875bc10f88cca0293351e
Yaniv Kremer <yanivkr7@gmail.com>
2020-10-06 19:08:38 +0300

    Adding shop page, collection-preview component and collection-item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
+
 const MenuItem = ({ title, imageUrl, size }) => (
 	<div className={`menu-item ${size}`}>
-		<div class="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
+		<div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
 		<div className="content">
 			<h1 className="title">{title.toUpperCase()}</h1>
 			<span className="subtitle">SHOP NOW</span>
@@ -10,4 +12,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 	</div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
